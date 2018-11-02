0bc916a9c8d265210202978378b9e7e478d01fd9
Tom Hodson <thomas.c.hodson@gmail.com>
2018-11-02 17:46:10 +0000

    update jobmanager

--- jobmanager.py
+++ jobmanager.py
@@ -22,7 +22,7 @@ def get_config(job_id, configs):
     Take a job number and a config with multiple values and index into the cartesian product of configurations
     use the value 'loop_over' to decide which values get looped over
     '''
-
+    assert(job_id < total_jobs(configs))
     #get all the keys that aren't looped over
     single_config= {k:v for k,v in configs.items() if k not in set(configs['loop_over'])}
 
@@ -41,8 +41,8 @@ def get_config(job_id, configs):
 
     return single_config
 
-def setup_mcmc(mcmc_routine, config, working_dir = Path('./')):
-    working_dir.mkdir(parents=True, exist_ok=True)
+def setup_mcmc(mcmc_routine, config, working_dir = Path('./'), overwrite = False):
+    working_dir.mkdir(parents=True, exist_ok=overwrite)
     (working_dir / 'jobs').mkdir(parents=True, exist_ok=True)
 
     names, vals, lens, dtypes = config_dimensions(config)
@@ -53,18 +53,40 @@ def setup_mcmc(mcmc_routine, config, working_dir = Path('./')):
 
     script = f'''
 #!/usr/bin/env bash
+#PBS -N MCMC_simulation
 #PBS -lselect=1:ncpus=1:mem=1gb
 #PBS -lwalltime=24:00:00
 #PBS -J 1-{total_jobs(config)}
+#PBS -m abe
+#PBS -M tch14@imperial.ac.uk
+#PBS -j oe
+#PBS -o {working_dir.resolve()}/logs/$PBS_JOBID
 
-module load intel-suite anaconda/personal
+echo ------------------------------------------------------
+echo -n 'Job is running on node '; cat $PBS_NODEFILE
+echo ------------------------------------------------------
+echo PBS: qsub is running on $PBS_O_HOST
+echo PBS: originating queue is $PBS_O_QUEUE
+echo PBS: executing queue is $PBS_QUEUE
+echo PBS: working directory is $PBS_O_WORKDIR
+echo PBS: execution mode is $PBS_ENVIRONMENT
+echo PBS: job identifier is $PBS_JOBID
+echo PBS: job name is $PBS_JOBNAME
+echo PBS: node file is $PBS_NODEFILE
+echo PBS: current home directory is $PBS_O_HOME
+echo PBS: PATH = $PBS_O_PATH
+echo ------------------------------------------------------
+
+module load intel-suite anaconda3/personal
+. /home/tch14/anaconda3/etc/profile.d/conda.sh
 conda activate idp
 
-run_mcmc --job-id $PBS_ARRAY_INDEX --working-dir /rds/general/user/tch14/home/Falicok-Kimball-Monte-Carlo/test_run
+run_mcmc --job-id $PBS_ARRAY_INDEX --working-dir {working_dir.resolve()}
     '''
 
     with open(working_dir / 'runscript.sh', 'w') as f:
         f.write(script)
+        print(script)
 
     result_filename = working_dir / "results.hdf5"
     with h5py.File(result_filename, "w") as result_file:
@@ -76,10 +98,16 @@ run_mcmc --job-id $PBS_ARRAY_INDEX --working-dir /rds/general/user/tch14/home/Fa
 @click.command()
 @click.option('--job-id', default=1, help='which job to run')
 @click.option('--working-dir', default='./', help='where to look for the config files')
-def run_mcmc(job_id, working_dir = Path('./'), overwrite = True, mcmc_routine = cython_mcmc):
+@click.option('--temp-dir', default='./', help='where to store temporary files')
+def run_mcmc(job_id, 
+             working_dir = Path('./'),
+             temp_dir = Path('./'),
+             overwrite = True, mcmc_routine = cython_mcmc):
     '''
     Does the work that a single thread is expected to do.
     '''
+    print(f'job_id == {job_id}')
+    
     working_dir = Path(working_dir)
     result_file = working_dir / "results.hdf5"
     if not result_file.exists():
@@ -90,7 +118,7 @@ def run_mcmc(job_id, working_dir = Path('./'), overwrite = True, mcmc_routine = 
         config = dict(f.attrs)
 
     starttime = time.time()
-    this_config = get_config(job_id, config)
+    this_config = get_config(job_id-1, config)
     names, results = mcmc_routine(**this_config)
     runtime = time.time() - starttime
 
@@ -101,18 +129,41 @@ def run_mcmc(job_id, working_dir = Path('./'), overwrite = True, mcmc_routine = 
 
     with h5py.File(job_file, "w") as f:
         f.attrs.update(this_config)
+        f.attrs['runtime'] = runtime
+            
         for name, result in zip(names, results):
             result_data = f.create_dataset(name, data=result)
-            result_data.attrs['runtime'] = runtime
 
+def job_completion(working_dir):
+    result_filename = working_dir / "results.hdf5"
+    job_dir =  working_dir / "jobs"
+
+    missing = []
+    with h5py.File(result_filename, "r+") as result_file:
+        config = dict(result_file.attrs)
+
+        for job_id in range(1,total_jobs(config)+1):
+            job_filename = job_dir / f"job_{job_id}.hdf5"
+            if not job_filename.exists():
+                missing.append(job_id)
+                
+        print(f'missing : {len(missing)} of {total_jobs(config)} total')
+        print(missing)
+            
 def gather_mcmc(working_dir, overwrite = True):
     result_filename = working_dir / "results.hdf5"
     job_dir =  working_dir / "jobs"
 
+    missing = 0
     with h5py.File(result_filename, "r+") as result_file:
         config = dict(result_file.attrs)
 
-        for job_filename in job_dir.iterdir():
+        for job_id in range(1,total_jobs(config)+1):
+            job_filename = job_dir / f"job_{job_id}.hdf5"
+            if not job_filename.exists():
+                missing += 1
+                continue
+            
             with h5py.File(job_filename, 'r') as job_file:
                 #loop over the datasets, ie energy, magnetisation etc
                 for dataset_name, val in job_file.items():
@@ -125,3 +176,4 @@ def gather_mcmc(working_dir, overwrite = True):
                         dim.label = name
 
                     dataset[indices] = val
+        print(f'missing : {missing} of {total_jobs(config)} total')
