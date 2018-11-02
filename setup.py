349a0b982f9f273ebf0019f12ab2781475596f11
TomHodson <thomas.c.hodson@gmail.com>
2018-11-02 10:33:52 +0000

    modify setup.py to work with the ICC compuler

--- setup.py
+++ setup.py
@@ -1,8 +1,29 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
+extensions = [
+    Extension("*", ["*.pyx"],
+        include_dirs=[numpy.get_include()],
+        extra_compile_args = ['-ip_no_inlining'], #required to disable ipo so that it will build with ICC
+        #libraries=[...],
+        #library_dirs=[...]
+        ),
+
+]
+
 setup(
     name="Falicov Kimball Monte Carlo",
-    ext_modules=cythonize("*.pyx", include_path = [numpy.get_include()], annotate=True),
+    py_modules=['jobmanager'],
+    ext_modules=cythonize(extensions, annotate=True),
+    install_requires=['Click'],
+    entry_points='''
+        [console_scripts]
+        run_mcmc=jobmanager:run_mcmc
+    ''',
 )
+
+
+
+#command to build inplace is: python setup.py build_ext --inplace
+#command to install is: pip install . --editable
